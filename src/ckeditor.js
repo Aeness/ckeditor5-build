e9f0193464b10469e14628d70b27beec7163a8ff
Aeness <124333+Aeness@users.noreply.github.com>
2021-05-01 13:34:30 +0200

    Use @ckeditor v26.0.0 and @aeness/ckeditor5-insert-image v26.0.1.

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -6,22 +6,22 @@ import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import AutoLink from '@ckeditor/ckeditor5-link/src/autolink';
 import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-import InsertImage from '@aeness/ckeditor5-insert-image/src/insertimage';
+import OnlyInsertImage from '@aeness/ckeditor5-insert-image/src/onlyinsertimage';
 
 export default class SimpleEditor extends ClassicEditor {}
 
@@ -33,20 +33,20 @@ SimpleEditor.builtinPlugins = [
     Underline,
     Alignment,
     BlockQuote,
-    EasyImage,
     Heading,
     Image,
-    InsertImage,
+    OnlyInsertImage,
     ImageResize,
     ImageStyle,
     ImageToolbar,
+	ImageCaption,
     AutoImage,
     Indent,
+    IndentBlock,
     Link,
     AutoLink,
     LinkImage,
     List,
-    Paragraph,
     PasteFromOffice,
     TextTransformation
 ];
@@ -71,7 +71,7 @@ SimpleEditor.defaultConfig = {
             'outdent',
             'blockQuote',
             '|',
-            'insertImage',
+            'onlyInsertImage',
             '|',
             'undo',
             'redo'
