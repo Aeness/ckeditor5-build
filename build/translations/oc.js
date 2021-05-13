0a413547b1c0954f9f4e172fa73ca6ff6b87e374
Aeness <124333+Aeness@users.noreply.github.com>
2021-05-13 19:54:11 +0200

    Use @aeness/ckeditor5-insert-image v27.0.1. + FIXE @ckeditor depedencies + npm update eslint, style-loader, webpack, webpack-cli

--- build/translations/oc.js
+++ build/translations/oc.js
@@ -1,1 +1,1 @@
-(function(d){	const l = d['oc'] = d['oc'] || {};	l.dictionary=Object.assign(		l.dictionary||{},		{Bold:"Gras",Cancel:"Anullar",Italic:"Italica",Save:"Enregistrar",Underline:""}	);l.getPluralForm=function(n){return (n > 1);;};})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
+!function(n){const i=n.oc=n.oc||{};i.dictionary=Object.assign(i.dictionary||{},{Bold:"Gras",Cancel:"Anullar",Italic:"Italica",Save:"Enregistrar",Underline:""}),i.getPluralForm=function(n){return n>1}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
